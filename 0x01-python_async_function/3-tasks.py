02506344e814a723a858c0a41c8a60b2f2f1baa4
Omar Jammeh <orms45@gmail.com>
2024-08-22 16:46:02 +0000

    Add 3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task_wait_random function"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+@asyncio.coroutine
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ task_wait_random(max_delay)
+    return a asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
