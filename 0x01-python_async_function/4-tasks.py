94f9e27bac3f5993638d894409feb274f7385682
Omar Jammeh <orms45@gmail.com>
2024-08-23 07:39:24 +0000

    Type annotation fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Function wait_n"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Wait spawn number of delays: n
     returns all the delays in a list
     """
